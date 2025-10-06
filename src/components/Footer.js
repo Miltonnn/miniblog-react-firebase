bcf39d6b46e765bf4b39cdbd52374e359695346e
Miltonnn <juninhoeufrasio2017@hotmail.com>
2025-10-06 19:14:57 -0300

    feat: links da barra de navegação

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import styles from "./Footer.module.css"
+
+const Footer = () => {
+    return (
+        <footer>
+            <h3>Escre sobre o que voce tem interesse!</h3>
+            <p>Mini Blog &copy; 2025</p>
+        </footer>
+    )
+}
+
+export default Footer
