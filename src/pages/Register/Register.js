64af5cc8074916eca7a7453d075c1917fb3d6764
Miltonnn <juninhoeufrasio2017@hotmail.com>
2025-10-07 20:04:20 -0300

    feat: fazendo a parte de autenticacao, enviando os dados do para o firebase

--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -1,87 +1,104 @@
-import styles from "./Register.module.css"
+import styles from "./Register.module.css";
 
-import { useState, useEffect } from "react"
+import { useState, useEffect } from "react";
+
+import { useAuthentication } from "../../hooks/useAuthentication";
 
 const Register = () => {
-    const [displayName, setDisplayName] = useState("");
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [confirmPasswod, setConfirmPasswod] = useState("");
-    const [error, setError] = useState("");
+  const [displayName, setDisplayName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPasswod, setConfirmPasswod] = useState("");
+  const [error, setError] = useState("");
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
+  const { createUser, error: authError, loading } = useAuthentication();
 
-        setError("")
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-        const user = {
-            displayName,
-            email,
-            password
-        }
+    setError("");
 
-        if (password !== confirmPasswod) {
-            setError("As senhas precisam ser iguais!")
-            return
-        }
+    const user = {
+      displayName,
+      email,
+      password,
+    };
 
-        console.log(user)
+    if (password !== confirmPasswod) {
+      setError("As senhas precisam ser iguais!");
+      return;
     }
 
-    return (
-        <div className={styles.register}>
-            <h1>Cadastre-se para postar</h1>
-            <p>Crie seu usuário e compartilhe suas histórias</p>
-            <form onSubmit={handleSubmit}>
-                <label>
-                    <span>Nome:</span>
-                    <input
-                        type="text"
-                        name="displayName"
-                        required
-                        placeholder="Nome do usuário"
-                        value={displayName}
-                        onChange={(e) => setDisplayName(e.target.value)}
-                    />
-                </label>
-                <label>
-                    <span>E-mail:</span>
-                    <input
-                        type="email"
-                        name="email"
-                        required
-                        placeholder="E-mail do usuário"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                    />
-                </label>
-                <label>
-                    <span>Senha:</span>
-                    <input
-                        type="password"
-                        name="password"
-                        required
-                        placeholder="Insira sua senha"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                    />
-                </label>
-                <label>
-                    <span>Confirmação de senha:</span>
-                    <input
-                        type="password"
-                        name="password"
-                        required
-                        placeholder="Confirma a sua senha"
-                        value={confirmPasswod}
-                        onChange={(e) => setConfirmPasswod(e.target.value)}
-                    />
-                </label>
-                <button className="btn">Cadastrar</button>
-                {error && <p className="error">{error}</p>}
-            </form>
-        </div>
-    )
-}
+    const res = await createUser(user);
 
-export default Register
+    console.log(user);
+  };
+
+  useEffect(() => {
+    if (authError) {
+      setError(authError);
+    }
+  }, [authError]);
+
+  return (
+    <div className={styles.register}>
+      <h1>Cadastre-se para postar</h1>
+      <p>Crie seu usuário e compartilhe suas histórias</p>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <span>Nome:</span>
+          <input
+            type="text"
+            name="displayName"
+            required
+            placeholder="Nome do usuário"
+            value={displayName}
+            onChange={(e) => setDisplayName(e.target.value)}
+          />
+        </label>
+        <label>
+          <span>E-mail:</span>
+          <input
+            type="email"
+            name="email"
+            required
+            placeholder="E-mail do usuário"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </label>
+        <label>
+          <span>Senha:</span>
+          <input
+            type="password"
+            name="password"
+            required
+            placeholder="Insira sua senha"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        <label>
+          <span>Confirmação de senha:</span>
+          <input
+            type="password"
+            name="password"
+            required
+            placeholder="Confirma a sua senha"
+            value={confirmPasswod}
+            onChange={(e) => setConfirmPasswod(e.target.value)}
+          />
+        </label>
+        {!loading && <button className="btn">Cadastrar</button>}
+        {loading && (
+          <button className="btn" disabled>
+            Aguarde...
+          </button>
+        )}
+        {error && <p className="error">{error}</p>}
+      </form>
+    </div>
+  );
+};
+
+export default Register;
