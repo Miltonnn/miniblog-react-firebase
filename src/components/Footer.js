f46e0dc9fafc27d7a4f83d3bfed4f7cbc340d118
Miltonnn <juninhoeufrasio2017@hotmail.com>
2025-10-07 10:45:50 -0300

    feat: zerando css e estilizando footer e header

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import styles from "./Footer.module.css"
 
 const Footer = () => {
     return (
-        <footer>
+        <footer className={styles.footer}>
             <h3>Escre sobre o que voce tem interesse!</h3>
             <p>Mini Blog &copy; 2025</p>
         </footer>
